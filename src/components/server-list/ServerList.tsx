a3a2af7b91e5127ec0cc72e863f4249b1b7aba67
joj <johnkorhel1@gmail.com>
2023-07-04 10:44:02 -0600

    update table

--- src/components/server-list/ServerList.tsx
+++ src/components/server-list/ServerList.tsx
@@ -1,72 +1,48 @@
 import * as React from 'react'
 import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid'
-import { styled, Box, Typography } from '@mui/material'
+import { styled, Box, TextField } from '@mui/material'
 import WbSunnyIcon from '@mui/icons-material/WbSunny'
 import NightsStayIcon from '@mui/icons-material/NightsStay'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 import CancelIcon from '@mui/icons-material/Cancel'
 import { ServerData, getServerData } from '../../api/servers.ts'
 
-const columns: GridColDef[] = [
-  { field: 'Name', headerName: 'Name' },
-  { field: 'Map', headerName: 'Map', width: 110 },
-  { field: 'MapSize', headerName: 'Map Size' },
-  { field: 'Gamemode', headerName: 'Game Mode', width: 110 },
-  { field: 'Region', headerName: 'Region', width: 150 },
-  { field: 'Players', headerName: 'Players' },
-  { field: 'QueuePlayers', headerName: 'Queue Players' },
-  { field: 'MaxPlayers', headerName: 'Max Players' },
-  { field: 'Hz', headerName: 'Hz' },
-  {
-    field: 'DayNight',
-    headerName: 'Day/Night',
-    renderCell: (params: GridRenderCellParams) => {
-      if (params.value === 'Day') {
-        return <WbSunnyIcon color='warning' />
-      }
-      if (params.value === 'Night') {
-        return <NightsStayIcon color='info' />
-      }
-      return params.value
-    },
-  },
-  {
-    field: 'IsOfficial',
-    headerName: 'Official',
-    renderCell: (params: GridRenderCellParams) =>
-      params.value ? <CheckCircleIcon color='success' /> : <CancelIcon color='error' />,
-  },
-  {
-    field: 'HasPassword',
-    headerName: 'Password',
-    renderCell: (params: GridRenderCellParams) =>
-      params.value ? <CheckCircleIcon color='success' /> : <CancelIcon color='error' />,
-  },
-  { field: 'AntiCheat', headerName: 'Anti Cheat' },
-  { field: 'Build', headerName: 'Build', flex: 2 },
-]
+
+const fields = ['Name', 'Map', 'MapSize', 'Gamemode', 'Region', 'Players', 'QueuePlayers', 'MaxPlayers', 'Hz', 'AntiCheat', 'Build', 'DayNight', 'IsOfficial', 'HasPassword']
+
+const checkOrCancel = ({ value }: GridRenderCellParams) => value ? <CheckCircleIcon color='success' /> : <CancelIcon color='error' />;
+
+const specialRenderCell = {
+  DayNight: ({ value }: GridRenderCellParams) => 
+    value === 'Day' ? <WbSunnyIcon color='warning' /> : <NightsStayIcon color='info' />,
+  IsOfficial: checkOrCancel,
+  HasPassword: checkOrCancel,
+};
+
+
+const columns: GridColDef[] = fields.map((field) => {
+  const column: GridColDef = { field, headerName: field, flex: 1 };
+  if (field in specialRenderCell) {
+    column.renderCell = specialRenderCell[field];
+  }
+  return column;
+});
+
 
 const DarkDataGrid = styled(DataGrid)(({ theme }) => ({
   '& .MuiDataGrid-root': {
     backgroundColor: theme.palette.background.default,
     color: theme.palette.text.primary,
   },
-  '& .MuiDataGrid-columnHeader': {
+  '& .MuiDataGrid-columnHeader, .MuiDataGrid-cell, .MuiDataGrid-footerContainer': {
     backgroundColor: theme.palette.background.paper,
     color: theme.palette.text.primary,
-  },
-  '& .MuiDataGrid-cell': {
-    backgroundColor: theme.palette.background.paper,
-    color: theme.palette.text.primary,
-  },
-  '& .MuiDataGrid-footerContainer': {
-    backgroundColor: theme.palette.background.paper,
-    color: theme.palette.text.primary,
-  },
+  }
 }))
 
 export default function ServerList() {
   const [rows, setRows] = React.useState<ServerData[]>([])
+  const [searchTerm, setSearchTerm] = React.useState('')
 
   React.useEffect(() => {
     getServerData().then((data) => {
@@ -75,17 +51,35 @@ export default function ServerList() {
     })
   }, [])
 
+  const filteredRows = rows.filter((row) => 
+    Object.values(row).some((value) =>
+      value?.toString().toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  )
+
   return (
     <Box m={2}>
-      <Typography variant='h5' color='#fff' gutterBottom>
-        Server List
-      </Typography>
+      <TextField 
+        label="Search" 
+        variant="outlined" 
+        onChange={(e) => setSearchTerm(e.target.value)}
+        sx={{marginBottom: 2, marginTop: 1, width: '100%'}}
+      />
       <DarkDataGrid
-        rows={rows}
+      componentsProps={{
+        toolbar: {
+            showQuickFilter: true,
+               quickFilterProps: { debounceMs: 500 },
+         },
+      }}
+        rows={filteredRows}
+        initialState={{ pagination: { paginationModel: { pageSize: 20 } } }}
         columns={columns}
         density='compact'
         disableColumnMenu
         disableRowSelectionOnClick
+        autoHeight
+        style={{ height: 'calc(100vh - 120px)', width: '100%' }}
       />
     </Box>
   )
